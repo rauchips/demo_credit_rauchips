847faeced74c7825f512403037b968a626aec88e
rauchips <chipindebenjamin@gmail.com>
2022-11-10 22:45:18 +0300

    feat: base project endpoint

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import helmet from 'helmet';
 import * as dotenv from 'dotenv';
-import { Application } from 'express';
+import { Application, Request, Response } from 'express';
 
 
 dotenv.config();
@@ -24,6 +24,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(usersRoute);
 app.use(walletsRoute);
 app.use(ErrorHandler);
+app.use("*", (req: Request, res: Response) => {
+  return res.status(200).json({ 
+    message: 'This is the base project visit the following endpoints',
+    user: {
+      'GET all users' : '/api/user',
+      'POST new user - signup' : '/api/user/signup',
+      'POST user - signin' : '/api/user/signin',
+
+    },
+    wallet: {
+      'GET all wallets' : '/api/wallet',
+      'GET wallet balance for user with id' : '/api/wallet/balance/:id',
+      'PATCH make deposit' : '/api/wallet/deposit/:id',
+      'PATCH make withdrawal' : '/api/wallet/withdraw/:id',
+      'PATCH make transfer' : '/api/wallet/transfer/:id',
+
+    }
+  });
+});
 
 if (NODE_ENV !== 'test') {
   app.listen(PORT, () => console.log(`Server is running on port:${PORT}`));
